5f772897b8c5f3e07b4eeb35cc36a7f5fc6658a1
RuchaMK <ruchasf@gmail.com>
2024-04-20 23:58:55 +0530

    LWC Superbadge Trailhead

---
+++ force-app/main/default/lwc/contactCreator/contactCreator.js
@@ -0,0 +1,23 @@
+import { LightningElement, wire } from 'lwc';
+import { showToastEvent } from 'lightning/platformShowToastEvent';
+import CONTACT_OBJECT from '@salesforce/schema/Contact'
+import FIRSTNAME_FIELD from '@salesforce/schema/Contact.FirstName'
+import LASTNAME_FIELD from '@salesforce/schema/Contact.LastName'
+import EMAIL_FIELD from '@salesforce/schema/Contact.Email'
+import getContacts from '@salesforce/apex/ContactController.getContacts';
+
+export default class ContactCreator extends LightningElement {
+    objectApiName = CONTACT_OBJECT
+    fields = [FIRSTNAME_FIELD, LASTNAME_FIELD, EMAIL_FIELD]
+
+    handleSuccess(event){
+        const toastMsg = new showToastEvent({
+            title: "Contact Record Created",
+            message: "Record Id: " + event.detail.id,
+            variant: "success"
+        })
+
+        this.dispatchEvent(toastMsg)
+    }
+
+}
